0c5a3c3cfa8bd8ac04ca893b74027ede394db64b
HTI-shivanidotcom <142248478+HTI-shivanidotcom@users.noreply.github.com>
2023-08-29 12:25:48 +0530

    Add files via upload

--- com.hsbc.business/Employee.java
+++ com.hsbc.business/Employee.java
@@ -1,1 +1,76 @@
+package com.hsbc.business;
 
+public class Employee {
+	private int empid;
+	private String empname;
+	private String city;
+	public Employee(int empid, String empname, String city) {
+//		super();
+		this.empid = empid;
+		this.empname = empname;
+		this.city = city;
+	}
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + empid;
+		result = prime * result + ((empname == null) ? 0 : empname.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (empid != other.empid)
+			return false;
+		if (empname == null) {
+			if (other.empname != null)
+				return false;
+		} else if (!empname.equals(other.empname))
+			return false;
+		return true;
+	}
+	
+	public int compareTo(Employee o) {
+		return Integer.compare(this.empid, o.empid);
+	}
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", empname=" + empname + ", city=" + city + "]";
+	}
+	
+	
+}
