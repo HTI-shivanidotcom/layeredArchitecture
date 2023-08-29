3affde2348e4ea1eedc0f59514133a65fea7a28e
HTI-shivanidotcom <142248478+HTI-shivanidotcom@users.noreply.github.com>
2023-08-29 12:28:25 +0530

    Add files via upload

--- com.hsbc.data/Storage.java
+++ com.hsbc.data/Storage.java
@@ -1,1 +1,14 @@
+package com.hsbc.data;
+import java.util.List;
 
+import com.hsbc.business.Employee;
+
+import Exceptions.EmployeeAlreadyExistsException;
+import Exceptions.EmployeeNotFoundException;
+
+public interface Storage {
+	void addEmployee(Employee e) throws EmployeeAlreadyExistsException;
+	Employee getEmployee(Employee e) throws EmployeeNotFoundException;
+	List<Employee> showEmployee();
+	String deleteEmployee(int empid) throws EmployeeNotFoundException;
+}
